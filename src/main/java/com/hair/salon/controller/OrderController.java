1260746b9d21088f3b62043dd98ffe8695d06b1b
in1234 <cyuuunn123@gmail.com>
2022-01-11 11:11:47 +0900

    예약확인

--- src/main/java/com/hair/salon/controller/OrderController.java
+++ src/main/java/com/hair/salon/controller/OrderController.java
@@ -1,5 +1,23 @@
 package com.hair.salon.controller;
 
-public class OrderController {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+@RequestMapping
+public class OrderController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
+	
+	@RequestMapping(value ="/resList", method =RequestMethod.GET)
+	public String resList () {
+		logger.info("resList 매서드 실행.(GET)");
+		
+		return "resList";
+	}
+	
+	
 }
